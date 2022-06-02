db954914fb7d80f085eadb8991180217b19532ef
Daniel Jonathan <daniel@cosmicmind.com>
2022-06-01 20:10:44 -0600

    feat(CV-0): wip new puzzles being added

---
+++ src/dynamic-programming/longest-increasing-subsequence.ts
@@ -0,0 +1,59 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, Daniel Jonathan <daniel at cosmicverse dot org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Longest Increasing Subsequence
+ *
+ * Given a non-empty array of integers, write a function that returns the
+ * longest strictly-increasing subsequence in the array.
+ *
+ * A subsequence of an array is a set of numbers that aren't necessarily
+ * adjacent in the array but that are in the same order as they appear in
+ * the array. For instance, the numbers [1, 3, 4] from a subsequence of the
+ * array [ 1, 2, 3, 4 ], and so do the number [ 2, 4 ]. Note that a single
+ * number in an array and the array itself are both valid subsequences of
+ * the array.
+ *
+ * You can assume that there will only be one longest increasing subsequence.
+ *
+ * Optimal Space & Time Complexity
+ * O(nlogn) time | O(n) space - where n is the length of the input array
+ *
+ * Sample Input
+ * (1) [ 5, 7, -24, 12, 10, 2, 3, 12, 5, 6, 35 ]
+ * (2) [ 100, 1, 2, 3, 4, 101 ]
+ */
+
+export function longestIncreasingSubsequence(array: number[]): number[] {
+
+  return [ -1 ]
+}
